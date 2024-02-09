0b577bbb281d8c6ffe36a980e7768609756ec0a0
kao-outar <kaoutar.arare@gmail.com>
2024-02-09 11:43:00 +0100

    l'Ajout du pokédex

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
+<?php
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/favicon.png">
+    <link rel="stylesheet" href="css/style.css">
     <title>Pokédex</title>
 </head>
 <body>
     <main>
-        <img src="" alt="">
+        <img src="images/Group 8.svg" alt="image pokédex">
     </main>
 </body>
 </html>
